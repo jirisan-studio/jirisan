898b52b33af11bb6335bd8254161c5613951d12e
jirisan-studio <nature.kr@gmail.com>
2025-12-11 21:02:50 +0900

    최종복구 - 구버전

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind, tailwindcss from '@tailwindcss/vite';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwind(), tailwindcss()],
-  },
+    plugins: [tailwindcss()]
+  }
 });
