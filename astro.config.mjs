ba01336bf50cc4f5e2629a148aa882daae7a4a53
jirisan-studio <nature.kr@gmail.com>
2025-12-11 20:16:31 +0900

    ss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@tailwindcss/vite';
+import tailwind, tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwind()],
+    plugins: [tailwind(), tailwindcss()],
   },
 });
